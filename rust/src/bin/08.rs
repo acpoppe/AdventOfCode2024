ba67cc73e0ef0363755052ac328c0b8c9dad4f8f
Allison Poppe <allison.poppe@quickbird.io>
2024-12-10 03:18:01 +0100

    Day 8

--- rust/src/bin/08.rs
+++ rust/src/bin/08.rs
@@ -20,7 +20,47 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let map = read_chars(input);
+    let antennas = parse_input(&map);
+    let mut antinodes: HashSet<(i64, i64)> = HashSet::new();
+    for frequency in antennas.keys() {
+        check_for_antinode_pt2(
+            &mut antennas.get(frequency).unwrap().clone(),
+            &mut antinodes,
+            map[0].len() as i64,
+            map.len() as i64,
+        );
+    }
+    Some(antinodes.len() as u64)
+}
+
+fn check_for_antinode_pt2(
+    antenna_locations: &mut Vec<(i64, i64)>,
+    antinodes: &mut HashSet<(i64, i64)>,
+    width: i64,
+    height: i64,
+) {
+    while antenna_locations.len() > 1 {
+        let (x1, y1) = antenna_locations.pop().unwrap();
+        for (x2, y2) in antenna_locations.iter() {
+            let dx = x2 - x1;
+            let dy = y2 - y1;
+            let mut offset_x = 0;
+            let mut offset_y = 0;
+            while is_in_bounds((x1 + offset_x, y1 + offset_y), width, height) {
+                antinodes.insert((x1 + offset_x, y1 + offset_y));
+                offset_x += dx;
+                offset_y += dy;
+            }
+            let mut offset_x = 0;
+            let mut offset_y = 0;
+            while is_in_bounds((x2 + offset_x, y2 + offset_y), width, height) {
+                antinodes.insert((x2 + offset_x, y2 + offset_y));
+                offset_x -= dx;
+                offset_y -= dy;
+            }
+        }
+    }
 }
 
 fn check_for_antinode(
@@ -71,14 +111,15 @@ fn is_valid_location(
     width: i64,
     height: i64,
 ) -> bool {
-    check.0 >= 0
-        && check.0 < width
-        && check.1 >= 0
-        && check.1 < height
+    is_in_bounds(check, width, height)
         && get_distance(first, check) != 0.0
         && get_distance(second, check) != 0.0
         && ((get_distance(first, check) / get_distance(second, check)) <= 0.5
             || (get_distance(first, check) / get_distance(second, check)) >= 2.0)
+}
+
+fn is_in_bounds(check: (i64, i64), width: i64, height: i64) -> bool {
+    check.0 >= 0 && check.0 < width && check.1 >= 0 && check.1 < height
 }
 
 fn get_distance(first: (i64, i64), second: (i64, i64)) -> f64 {
@@ -110,6 +151,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(34));
     }
 }
